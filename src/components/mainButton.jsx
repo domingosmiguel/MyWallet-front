f1b58c22dae9f801c10bad6c1be43c7016776aa6
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2023-12-12 08:29:47 -0300

    fix: css button color specificity problem

--- src/components/mainButton.jsx
+++ src/components/mainButton.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
 import { Spinner } from '@chakra-ui/react';
+import styled from 'styled-components';
 
 export default function MainButton({ children, isLoading, onClick }) {
   return (
@@ -27,7 +27,7 @@ const StyledButton = styled.button`
   height: 2.875rem;
   font-family: ${({ theme }) => theme.fonts.body};
   background-color: ${({ theme }) => theme.colors.lightMain};
-  color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.secondary} !important;
   border-radius: 0.5rem;
   font-size: 1.25rem;
   font-weight: 700;
